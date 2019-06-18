8debfee1a231cc8fae0bc8e0a442de4a40ff6f2e
alekseykashtanov <alexcast.msu@mail.ru>
2019-06-18 22:07:57 +0300

    Переделана генерация ВВМ

--- discrete_source.py
+++ discrete_source.py
@@ -5,17 +5,31 @@
 
 import json
 from fractions import Fraction
-from math import log, log2, fabs
-from sympy import Symbol, solve, log2
+from math import log2
 
 
-def log_solver(R, p):
-	x = Symbol('x')
-	return solve(-(p + x) * log2(p + x) - (1 - p - x) * log2(1 - p - x) - R)
+def gen_code(q, size):
+    result = []
+    code_len = 0
+    while q ** code_len < size:
+        code_len += 1
+    for i in range(size):
+        vec = ""
+        elem = i
+        for j in range(code_len):
+            vec = str(elem % q) + vec
+            elem //= q
+        result.append(vec)
+    return result
 
 
-def local_entropy(p):
-	return - (p * log2(p) + (1 - p) * log2(1 - p))
+def print_encoding(hps, n, q):
+    code = gen_code(q, len(hps))
+    print("Кодирование: ")
+    print("{:<10}\t{:<10}".format("<HPS_ELEM>", "<ENCODING_WORD>"))
+    for i in range(len(hps)):
+        print("{:<10}\t{:<10}".format(hps[i], code[i]))
+
 
 
 def check_correctness(desc):
@@ -27,53 +41,52 @@ def check_correctness(desc):
 
 
 class DiscreteSource:
-    # src : dict - Description of DS
+    # src : dict    - Описание источника
+    # ent : float   - энтропия источника
 
     def __init__(self, desc):
         self.src = json.load(desc)
         check_correctness(self.src)
+        for key in self.src:
+            self.src[key] = float(Fraction(self.src[key]))
         self.probabilities = self.src.values()
+        self.ent = self.entropy()
 
     def entropy(self):
         return_ = 0
-        capacity = len(self.probabilities)
         for i in self.probabilities:
             if i:   # Дополнение в нуле по непрерывности
-                return_ -= i * log(i, capacity)
+                return_ -= i * log2(i)
         return return_
 
-    def gen_encoding(self, R, eps, q, out_file):
-    	# Проверка условий
-        if len(self.probabilities) != 2:
-            print("Не мучайте меня, дайте два!!")
-            return None
-        # Насильное изменение вероятностей
-        # Ну хочется, чтоб по теории
-        if self.src["1"] > 0.5:
-        	self.src["1"], self.src["0"] = self.src["0"], self.src["1"]
-        p = src["1"]
-        if R <= entropy():
-        	print("Невозможно сгенерировать код для такой скорости")
-        	return None
+    def calc_prob(self, hps):
+        p = 0
+        for elem in hps:
+            cur_p = 1
+            for bit in bin(elem)[2:]:
+                cur_p *= self.src[bit]
+            p += cur_p
+        return p
 
-        # Определение нужного индекса n для последовательности T_n
-        local_eps = log_solver(R, p)
+    # HPS - высоковероятное множество
+    def generate_hps(self, eps, n):
+        result = []
+        left_b = self.ent - eps
+        right_b = self.ent + eps
+        for elem in range(2 ** n):
+            ones = bin(elem).count('1')
+            p = self.src["1"] ** ones * self.src["0"] ** (n - ones)
+            if left_b <= -log2(p) / n <= right_b:
+                result.append(elem)
+        print(result)
+        return result
+
+    def gen_encoding(self, R, eps, q):
         n = 1
-        expr_ = p * (1 - p) / n * local_eps
-        while (expr_ < eps):
-        	n += 1
-        	expr_ = p * (1 - p) / n * local_eps
-
-        # Вычисление мощности множества T_n
-        capacity = 0
-        for i in range(2 ** n):
-        	k = bin(i).count('1')
-        	if fabs(k - n * p) < n * local_eps:
-        		capacity += 1
-
-        # Определение длины кодового слова
-       	m = 1
-       	while (q ** m < capacity):
-       		m += 1
-
-        return m
+        while True:
+            print('n = ', n)
+            hps_n = self.generate_hps(eps, n)
+            if self.calc_prob(hps_n) >=  1 - (R - self.ent):
+                break
+            n += 1
+        print_encoding(hps_n, n, q)
